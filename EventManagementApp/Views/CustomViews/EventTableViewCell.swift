1f4d9fc89b6e234897e77c2a5d61c1c532bd43a7
Melinda Murillo <67812236+melivinelli333@users.noreply.github.com>
2022-06-26 00:32:21 -0700

    second attempt at this assessment after accidentally deleting my last one with git hard reset head

---
+++ EventManagementApp/Views/CustomViews/EventTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EventTableViewCell.swift
+//  EventManagementApp
+//
+//  Created by Melinda Murillo on 6/25/22.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
